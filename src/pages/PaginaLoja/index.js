4259d554f1bfa7fc9651a5130d68d0fca6614a8c
meticulousfan <elithofan@gmail.com>
2022-11-19 18:41:04 -0300

    Pagina de loja e register loja

--- src/pages/PaginaLoja/index.js
+++ src/pages/PaginaLoja/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdStoreMallDirectory, MdStarBorder } from 'react-icons/md';
 import { TfiComments } from 'react-icons/tfi';
 
 import { Pagina } from './styled';
+import axios from '../../services/axios';
 
 export default function PaginaLoja() {
+    const [dadosLoja, setDadosLoja] = useState([]);
+
+    const DadosLoja = async () => {
+        try {
+            const url = `/loja/${localStorage.getItem('loja_id')}`;
+            const res = await axios.get(url);
+            setDadosLoja(res.data);
+            console.log(dadosLoja.produtos);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        DadosLoja();
+    }, []);
     return (
         <Pagina>
             <header>
-                <MdStoreMallDirectory size={100} />
-                <h5>Nome fantasia</h5>
+                {dadosLoja.url_imagem ? (
+                    <img
+                        src={dadosLoja.url_imagem}
+                        alt="foto de perfil da loja"
+                    />
+                ) : (
+                    <MdStoreMallDirectory size={100} />
+                )}
+
+                <h5>{dadosLoja.nome_fantasia}</h5>
             </header>
             <main>
                 <h5>Produtos vendidos pela loja:</h5>
