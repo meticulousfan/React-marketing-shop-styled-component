d3b8eb7f68580ea01c90fa310b138f6b2e0d570d
meticulousfan <elithofan@gmail.com>
2022-11-27 13:04:42 -0300

    Perfil usuario

--- src/pages/PerfilUsuario/DadosPerfil/index.js
+++ src/pages/PerfilUsuario/DadosPerfil/index.js
@@ -1,29 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import { Dados } from './styled';
 import axios from '../../../services/axios';
 
 export default function DadosPerfil() {
-    const [nome, setNome] = useState([]);
-    const [sobrenome, setSobrenome] = useState([]);
+    const usuario = useSelector((state) => state.auth.usuario);
 
-    const DadosUsuario = async () => {
-        try {
-            const url = `/usuario/${localStorage.getItem('usuario_id')}`;
-            const res = await axios.get(url);
-            console.log();
-        } catch (err) {
-            console.log(err);
+    const [Adresses, setAdreses] = useState([]);
+
+    useEffect(() => {
+        async function getAddress() {
+            const res = await axios.get(`/usuario-endereco/${usuario.id}`);
+            setAdreses(res);
+            console.log(res.data);
         }
-    };
+
+        getAddress();
+    }, [usuario.id]);
+
     return (
         <Dados className="overflow-auto">
             <title>
-                <h1>Bem-vindo (a)</h1>
+                <h1>Bem-vindo {usuario.nome}</h1>
             </title>
             <div className="nome">
-                <h5>Nome:</h5>
-                <h5>Sobrenome:</h5>
+                <h5>
+                    Nome: {usuario.nome} {usuario.sobrenome}
+                </h5>
             </div>
             <div className="info">
                 <h5>Informações de contato:</h5>
