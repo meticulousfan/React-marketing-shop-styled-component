359ba5adf15294f27e34cf98edcdd30a0afbe0f7
meticulousfan <elithofan@gmail.com>
2022-11-27 20:21:59 -0300

    Pagina de Perfil loja

--- src/pages/PerfilLoja/MeusProdutos/index.js
+++ src/pages/PerfilLoja/MeusProdutos/index.js
@@ -6,17 +6,32 @@ import { Produtos } from './styled';
 
 export default function MeusProdutos() {
     const loja = useSelector((state) => state.authLoja.loja);
-    console.log(loja);
+
+    function handleFormat(produto) {
+        const item = {
+            id: produto.produto_id,
+            imagens: produto.imagens_produto,
+            nome: produto.nome,
+            descricao: produto.descricao,
+            qtd: produto.qtd_estoque,
+            cores: produto.cor_produto,
+            tamanho: produto.tamanho_produto,
+            valor: produto.valor,
+        };
+        return item;
+    }
+
     return (
-        <Produtos>
+        <Produtos className="overflow-auto">
             <title>
                 <h1>Meus Produtos</h1>
             </title>
-            {/* {loja.produtos.map
-                <div className="overflow-auto">
-                    <CardProduto />
-                </div>
-            } */}
+            <div>
+                {loja.produtos.map((produto, index) => {
+                    const item = handleFormat(produto);
+                    return <CardProduto key={index} item={item} />;
+                })}
+            </div>
         </Produtos>
     );
 }
